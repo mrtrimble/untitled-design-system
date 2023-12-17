08cd070220ad55b70a568cab74b0601318cbd1c7
Ryan Trimble <mrtrimble2@gmail.com>
2023-12-17 13:14:59 -0500

    working using sass in parallel

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+// vite.config.ts
+
+import { browserslistToTargets } from 'lightningcss';
+import browserslist from 'browserslist';
+
+export default {
+  css: {
+    transformer: 'lightningcss',
+    lightningcss: {
+      targets: browserslistToTargets(browserslist('>= 0.25%')),
+    },
+  },
+  build: {
+    cssMinify: 'lightningcss',
+  },
+};
