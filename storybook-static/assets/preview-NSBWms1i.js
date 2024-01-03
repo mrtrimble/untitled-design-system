a3dae80087305a6bd0e8d540caf1ab8bca03a247
Ryan Trimble <mrtrimble2@gmail.com>
2024-01-03 16:08:10 -0500

    removes storybook-static`

--- storybook-static/assets/preview-NSBWms1i.js
+++
@@ -1,7 +0,0 @@
-import{_ as e}from"./iframe-3CM6wQKo.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-KRYuWees.js"),__vite__mapDeps([0,1,2,3,4,5,6]),import.meta.url);return new r}}};export{a as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-KRYuWees.js","./iframe-3CM6wQKo.js","./index-7Zy8PJUy.js","./_commonjsHelpers-5-cIlDoe.js","./index-ogXoivrg.js","./index-8LwkIDqE.js","./index-PPLHz8o0.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
